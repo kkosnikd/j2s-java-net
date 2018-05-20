bd638342587cc348165e845c3f5187ff0d2cb9b0
kevin downs <pyrosrppltoo@gmail.com>
2018-05-20 10:33:30 -0700

    up

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,19 +6,20 @@ object Dependencies {
 
   //List of Versions
   val V = new {
-    val scalaTest                 = "3.0.5"
-    val seeds                     = "4.0.7"
+    val scalaTest                   = "3.0.5"
+    val seeds                       = "4.0.7"
   }
 
   //List of Dependencies
-  val seeds                       = "zeab" %% "seeds" % V.seeds
-
-  val scalaTest                   = "org.scalatest" %% "scalatest" % V.scalaTest % "test"
+  val D = new {
+    val scalaTest                   = "org.scalatest" %% "scalatest" % V.scalaTest % "test"
+    val seeds                       = "zeab" %% "seeds" % V.seeds
+  }
 
   //Group Common Dependencies
   val commonDependencies: Seq[ModuleID] = Seq(
-    seeds,
-    scalaTest
+    D.seeds,
+    D.scalaTest
   )
 
 }
